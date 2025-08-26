8d9355c08841880aab63bad90eeb0831a776c883
aqsa <aqsa70729@gmail.com>
2025-08-26 16:21:14 +0500

    changes made to it

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import {defineConfig} from "vite"
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        questionPage: 'questionPage.html',
+        moviePage: 'moviePage.html',
+      },
+    },
+  },
+});
